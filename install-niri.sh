2452cde2ad30f119089df6ee1b5ca1a9e7315c6f
rael2pac <rael2pac@gmail.com>
2026-07-03 22:32:33 -0300

    fix(theme): add xsettingsd for global icon theme propagation

--- install-niri.sh
+++ install-niri.sh
@@ -186,7 +186,7 @@ OFFICIAL_PACKAGES=(
   pipewire pipewire-pulse pipewire-alsa pipewire-audio wireplumber
   grim slurp wl-clipboard fuzzel playerctl brightnessctl libnotify
   linux-lts-headers linux-zen-headers
-  nwg-look
+  nwg-look xsettingsd
   noto-fonts noto-fonts-emoji ttf-dejavu
   ntfs-3g exfatprogs dosfstools btrfs-progs xfsprogs
   jfsutils f2fs-tools udftools e2fsprogs gvfs
